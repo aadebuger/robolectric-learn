2c81af8d545bf0415035b8cbdd72279733aa05cd
aadebuger <admin@local.host>
2014-06-05 23:14:07 +0800

    first commit

---
+++ src/test/java/com/aadebuger/app/mcrmrlearn/HelloAndroidActivityTest.java
@@ -0,0 +1,23 @@
+package com.aadebuger.app.mcrmrlearn;
+
+import android.app.Activity;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class HelloAndroidActivityTest {
+
+  @org.junit.Test
+  public void titleIsCorrect() throws Exception {
+    Activity activity = Robolectric.setupActivity(HelloAndroidActivity.class);
+    assertTrue(activity.getTitle().toString().equals("Deckard"));
+  }
+  @org.junit.Test
+  public void titleIsCorrectError() throws Exception {
+    Activity activity = Robolectric.setupActivity(HelloAndroidActivity.class);
+    assertTrue(activity.getTitle().toString().equals("Deckard1"));
+  }
+}
