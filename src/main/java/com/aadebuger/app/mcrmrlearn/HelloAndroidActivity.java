7ecbcbbd6ff8e3ec0ced7f87f19b1bb9bce8c885
aadebuger <admin@local.host>
2014-06-05 23:49:03 +0800

    setTitle

--- src/main/java/com/aadebuger/app/mcrmrlearn/HelloAndroidActivity.java
+++ src/main/java/com/aadebuger/app/mcrmrlearn/HelloAndroidActivity.java
@@ -17,6 +17,7 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Deckard");
     }
 
     @Override
